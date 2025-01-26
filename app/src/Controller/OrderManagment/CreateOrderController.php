ba18e9a24b7d44f5f9cbefbdf4f44de06c87f767
Nicolas Filippo Farruggia <nicolasfilippo.farruggia@studenti.unipr.it>
2025-01-26 12:27:40 +0100

    Order Managment: allows creating an order #8 Work in progresst building up create order endpoint api

---
+++ app/src/Controller/OrderManagment/CreateOrderController.php
@@ -0,0 +1,17 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class CreateOrderController
+{
+    // TODO: build the create order endpoint
+
+    public function createOrder(): Response
+    {
+        return new Response(
+            '<html><body>TO DO</body></html>'
+        );
+    }
+}
