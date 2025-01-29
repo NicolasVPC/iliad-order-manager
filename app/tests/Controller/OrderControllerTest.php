f65ed60786ac5f4303bdcc1f381b68dfcdc6c267
NicolasVPC <nicofarru2000@gmail.com>
2025-01-29 02:22:00 +0100

    Allow editing an existing order #16

--- app/tests/Controller/OrderControllerTest.php
+++ app/tests/Controller/OrderControllerTest.php
@@ -87,4 +87,58 @@ final class OrderControllerTest extends WebTestCase
         $deleteResponseData = json_decode($deleteResponseContent, true);
         self::assertSame("Order with ID {$orderId} deleted successfully!", $deleteResponseData['message']);
     }
+
+    public function testUpdateOrder(): void
+    {
+        $client = static::createClient();
+
+        $timestamp = (new \DateTime())->getTimestamp();
+        $formattedDate = (new \DateTime())->setTimestamp($timestamp)->format('Y-m-d');
+
+        $client->request(
+            'POST',
+            '/create/order',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode([
+                'name' => 'Order to Update',
+                'description' => 'This order will be updated in the test.',
+                'date' => $formattedDate,
+                'products' => [1, 2],
+            ])
+        );
+
+        self::assertResponseIsSuccessful();
+        $responseContent = $client->getResponse()->getContent();
+        $responseData = json_decode($responseContent, true);
+        self::assertArrayHasKey('order_id', $responseData);
+
+        $orderId = $responseData['order_id'];
+
+        $updatedData = [
+            'order_id' => $orderId,
+            'name' => 'Updated Order Name',
+            'description' => 'Updated description',
+            'date' => (new \DateTime())->modify('+1 day')->format('Y-m-d'),
+            'products' => [3, 4],
+        ];
+
+        $client->request(
+            'PUT',
+            '/update/order',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($updatedData)
+        );
+
+        self::assertResponseIsSuccessful();
+        self::assertSame(200, $client->getResponse()->getStatusCode());
+        self::assertJson($client->getResponse()->getContent());
+
+        $updateResponseContent = $client->getResponse()->getContent();
+        $updateResponseData = json_decode($updateResponseContent, true);
+        self::assertSame("Order with ID {$orderId} updated successfully!", $updateResponseData['message']);
+    }
 }
